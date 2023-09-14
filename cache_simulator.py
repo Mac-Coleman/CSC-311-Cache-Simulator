3f624d15206a9a5325dfc4158695f4953faf42c0
Mac-Coleman <williamcormaccoleman@gmail.com>
2023-09-14 12:40:19 -0500

    Added main function

---
+++ cache_simulator.py
@@ -0,0 +1,10 @@
+"""
+The top-level organization for the main program logic.
+This will orchestrate the simulator and its components.
+"""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
