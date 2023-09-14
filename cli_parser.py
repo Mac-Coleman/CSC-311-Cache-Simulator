5ff166817441a63ed1edcb09d2688f6db5f0d764
Mac-Coleman <williamcormaccoleman@gmail.com>
2023-09-14 17:04:20 -0500

    Added string parsing to integers

--- cli_parser.py
+++ cli_parser.py
@@ -31,9 +31,20 @@ def parse_arguments(args: list[str]):
     block_size_parsed = get_option_value(args, consumed, "--block-size", "-b")
     cache_size_parsed = get_option_value(args, consumed, "--cache-size", "-c")
 
-    print(memory_size_parsed, block_size_parsed, cache_size_parsed)
+    if memory_size_parsed:
+        memory_size = memory_size_parsed
+    
+    if block_size_parsed:
+        block_size = block_size_parsed
+    
+    if cache_size_parsed:
+        cache_size = cache_size_parsed
 
     check_unconsumed(args, consumed)
+
+    print("Memory:", str_to_size(memory_size))
+    print("Block:", str_to_size(block_size))
+    print("Cache:", str_to_size(cache_size))
 
 def get_flag_presence(args: list[str], consumed: list[bool], long: str, short: str) -> bool:
     
@@ -110,6 +121,45 @@ def check_unconsumed(args: list[str], consumed: list[bool]):
     if len(unconsumed) > 0:
         print("Error: Unrecognized command line arguments.")
         print("The following command line inputs could not be interpreted: ", end="")
-        print(str(unconsumed)[1:-1])
+        print(str(unconsumed)[1:-1]) # Don't print the square brackets.
         print(f"Run 'python {args[0]} --help' to learn more.")
         sys.exit(1)
+
+def str_to_size(string: str) -> int:
+    """
+    Uses a string to produce an integer size.
+    A multiplier can be specified as a single character.
+
+    Raises ValueError if an empty string is passed.
+    Raises ValueError if the string can not be parsed.
+    Raises KeyError if a bad multiplier is used.
+    """
+
+    if len(string) == 0:
+        raise ValueError()
+
+    suffix_start: int = 0
+
+    while string[suffix_start] in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+        suffix_start += 1
+    
+    value = int(string[:suffix_start])
+    multiplier = 1
+
+    if suffix_start < len(string):
+
+        multiplier_dict = {
+            "B": 1,
+            "K": 2**10,
+            "M": 2**20,
+            "G": 2**30,
+            "T": 2**40,
+            "P": 2**50,
+            "E": 2**60,
+            "Y": 2**70,
+            "Z": 2**80,
+        }
+
+        multiplier = multiplier_dict[string[suffix_start].upper()]
+    
+    return value * multiplier
