a5be9dd251a92cda9292f04e6e73172222605eca
TdgCC <85917263+TdgCC@users.noreply.github.com>
2023-09-14 14:35:18 -0500

    fixed simulator with updated plans

--- simulator.py
+++ simulator.py
@@ -1,10 +1,20 @@
 
 from address_generator import AddressGenerator 
 def simulator(max_size:int, page_size:int, cache_size:int, map_algorithm, set_size:int, reads:int):
-    hit_record=list[bool]
     #cache=cache()
+    #output_builder=OutputBuilder()
+    hit_counter=0
+    total_counter=0
     address_maker=AddressGenerator(max_size)
     for i in reads:
         address=address_maker.generate_address(max_size)
-        #hit_record.append(cache.read(address))
+        hit=None#cache.read(address)
+        #outputbuilder.append(hit)
+        if(hit):
+            hit_counter+=1
+        total_counter+=1
+    hit_ratio=hit_counter/total_counter
     #replacements=cache.getReplacementCount()
+    #output_builder.close()
+    #output_builder.print_data(hit_ratio,locality,replacements)
+    #output builder make output from replacements and hit record 
