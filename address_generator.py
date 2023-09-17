697c357a3e7090496caba28a6e34fe2dd3201e57
TdgCC <85917263+TdgCC@users.noreply.github.com>
2023-09-17 15:44:14 -0500

    Update address_generator.py

--- address_generator.py
+++ address_generator.py
@@ -1,7 +1,7 @@
 import random
 
 class AddressGenerator:
-    def __init__(self,ms:int,pattern:int,page_size:int):
+    def __init__(self,ms:int,page_size:int,pattern:int):
         self.pattern=pattern
         self.max_size=ms
         self.pointer=0
@@ -14,14 +14,19 @@ class AddressGenerator:
     def generate_address(self):
         return self.patternDict.get(self.pattern)()
     def get_random_page(self):
-        return random.randint(0,self.max_size/self.page_size)
+        return random.randint(0,self.max_size//self.page_size-1)*self.page_size
     #different algorithms for generating input patterns 
     def random_pattern(self):
         return random.randint(0,self.max_size)
     def read_full_page(self):
         if(self.pointer==self.limit):
             self.pointer=self.get_random_page()
-            self.limit=self.pointer+self.page_size
+            self.limit=self.pointer+self.page_size-1
         else:
-            pointer+=1
-        return pointer
+            self.pointer+=1
+        return self.pointer
+    
+if __name__=="__main__":
+    ag=AddressGenerator(256,16,1)
+    for i in range(128):
+        print(ag.generate_address())
