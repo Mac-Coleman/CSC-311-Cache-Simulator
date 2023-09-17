7323a617f7d8f08d0319b119822749882357c528
Mac-Coleman <williamcormaccoleman@gmail.com>
2023-09-16 23:02:28 -0500

    Fixed progress bar formatting

--- simulator.py
+++ simulator.py
@@ -2,10 +2,10 @@
 from address_generator import AddressGenerator
 from cache import Cache, DirectCache, AssociativeCache, SetAssociativeCache
 from output_builder import OutputBuilder
-
 import ansi_terminal as cursor
 
 import time
+import math
 
 def simulate(max_size:int, page_size:int, cache_size:int, set_size:int, reads:int, replacement_algorithm: str):
     cache = SetAssociativeCache(page_size, cache_size, max_size, "lru", set_size)
@@ -13,6 +13,8 @@ def simulate(max_size:int, page_size:int, cache_size:int, set_size:int, reads:in
     hit_counter = 0
     total_counter = 0
     address_maker = AddressGenerator(max_size)
+
+    address_length = math.ceil(math.log(max_size, 16))
 
     start = time.perf_counter()
 
@@ -31,30 +33,34 @@ def simulate(max_size:int, page_size:int, cache_size:int, set_size:int, reads:in
             cursor.move_up()
             cursor.move_up()
             cursor.erase()
-            print(f"\rA: {address:016x}, hit: ", end="")
+            print("\rA: {a:0{width}x}\t".format(a=address, width=address_length), end="")
             cursor.green() if hit else cursor.red()
-            print(f"{hit:b}", end="")
+            print(f"{' hit' if hit else 'miss'}", end="")
             cursor.reset()
-            print(f", hit ratio: {hit_counter/total_counter * 100 :0.2f}%")
-            
-            cursor.yellow()
+            print(f"\thit ratio: {hit_counter/total_counter * 100 :0.2f}%")
+
             cursor.erase()
-            print(f"[{'='*int(i/reads * 20)}{' '*int((1 - (i/reads)) * 20)}] Progress: {i/reads * 100:0.2f}%", end="\n")
+            print("[", end="")
+            cursor.yellow()
+            print(f"{'='*int(i/reads * 20)}{' '*int((1 - (i/reads)) * 20)}", end="")
             cursor.reset()
+            print(f"] Progress: {i/reads * 100:0.2f}%", end="\n")
     
     cursor.move_up()
     cursor.move_up()
     cursor.erase()
-    print(f"\rA: {address:016x}, hit: ", end="")
+    print("\rA: {a:0{width}x}\t".format(a=address, width=address_length), end="")
     cursor.green() if hit else cursor.red()
-    print(f"{hit:b}", end="")
+    print(f"{' hit' if hit else 'miss'}", end="")
     cursor.reset()
-    print(f", hit ratio: {hit_counter/total_counter * 100 :0.2f}%")
+    print(f"\thit ratio: {hit_counter/total_counter * 100 :0.2f}%")
     
-    cursor.green()
     cursor.erase()
-    print(f"[{'='*int(i/reads * 20)}{' '*int((1 - (i/reads)) * 20)}] Progress: {i/reads * 100:0.2f}%", end="\n")
+    print("[", end="")
+    cursor.green()
+    print(f"{'='*int(i/reads * 20)}{' '*int((1 - (i/reads)) * 20)}", end="")
     cursor.reset()
+    print(f"] Progress: {100:0.2f}%", end="\n")
 
     hit_ratio = hit_counter / total_counter
     replacements = cache.get_replacement_count()
