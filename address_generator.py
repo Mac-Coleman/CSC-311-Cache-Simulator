914369e39e8b79f0a5f1ca8b58fb78554c4d02e4
TdgCC <85917263+TdgCC@users.noreply.github.com>
2023-09-18 10:14:24 -0500

    Update address_generator.py

--- address_generator.py
+++ address_generator.py
@@ -2,6 +2,7 @@ import random
 
 class AddressGenerator:
     def __init__(self,ms:int,page_size:int,pattern:int):
+        self.probability=.2
         self.pattern=pattern
         self.max_size=ms
         self.pointer=0
@@ -11,12 +12,15 @@ class AddressGenerator:
             0:self.random,
             1:self.read_full_page,
             2:self.random_sequential,
-            3:self.full_ram_sequential
+            3:self.full_ram_sequential,
+            4:self.probability_based_locality,
         }
     def generate_address(self):
         return self.patternDict.get(self.pattern)()
     def get_random_page(self):
         return random.randint(0,self.max_size//self.page_size-1)*self.page_size
+    def set_probability(self,prob:int):
+        self.probability=prob
     #different algorithms for generating input patterns 
     def random(self):
         return random.randint(0,self.max_size-1)
@@ -41,10 +45,20 @@ class AddressGenerator:
         else:
             self.pointer+=1
         return self.pointer
+    def probability_based_locality(self):
+        if(self.pointer==self.limit):
+            self.pointer=self.get_random_page()
+            self.limit=-1# this is the case for the first call only, after that it should be based on probability
+        else:
+            if(random.random()>self.probability):
+                self.pointer=self.get_random_page()
+        return random.randint(self.pointer,self.pointer+self.page_size-1)
+
+
 
 
 
 if __name__=="__main__":
-    ag=AddressGenerator(256,16,3)
+    ag=AddressGenerator(256,16,4)
     for i in range(128):
         print(ag.generate_address())
